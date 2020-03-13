e12f9a166aa2c92b954ff425879753e69449ba90
profaddy <adnansaify11@gmail.com>
2020-03-13 20:38:05 +0530

    updated entry flow completed

--- server/api/routes/entries.js
+++ server/api/routes/entries.js
@@ -5,113 +5,167 @@ const Entry = require("../models/entry");
 const Stock = require("../models/stock");
 const User = require("../models/user");
 const Product = require("../models/product");
-
 const mongoose = require("mongoose");
 const formatResponse = require("../utils/formatResponse");
 
-router.get(("/"),(req, res, next) => {
-    Entry.find().exec().then(result => {
-        res.status(200).json(formatResponse(true,"entries retrieved successfully",{entries:result}));
-    }).catch(error => {
-        res.status(500).json(formatResponse(false,`error occured while retrieving entries: ${error}`))
-    });
-});
-
-router.post(("/"),async (req, res, next) => {
-    let product = [];
-    let user = [];
-    let stock = [];
-    let remaining = 0;
-    let taken = undefined;
-    let consumed = undefined;
-    let returned = undefined;
-
-    await Product.findById(req.body.product_id).exec().then(result => {
-        product = result;
-    }).catch(error => {
-        console.log(error)
-    });
-
-    await User.findById(req.body.user_id).exec().then(result => {
-        user = result;
-    }).catch(error => {
-        console.log(error)
-    });
-
-    await Stock.findOne({product_id:req.body.product_id,user_id:req.body.user_id  }).exec().then(result => {
-        stock = result;
-    }).catch(error => {
-        console.log(error)
-    });
-    if(req.body.entry_type === "taken"){
-        remaining = Number(stock.bag_value) + Number(req.body.entry_value);
-        taken = Number(req.body.entry_value)
-    }else if(req.body.entry_type === "consumed"){
-        remaining = Number(stock.bag_value) - Number(req.body.entry_value);
-        consumed = Number(req.body.entry_value)
-    }else if(req.body.entry_type === "returned"){
-        remaining = Number(stock.bag_value) - Number(req.body.entry_value);
-        returned = Number(req.body.entry_value)
-    }else{
-        remaining = Number(stock.bag_value)
+getBagValue = (payload, currentBagValue) => {
+    try {
+        let remaining = 0;
+        let taken = 0;
+        let consumed = 0;
+        let returned = 0;
+        if (payload.entry_type === "taken") {
+            remaining = Number(currentBagValue) + Number(payload.entry_value);
+            taken = Number(payload.entry_value)
+        } else if (payload.entry_type === "consumed") {
+            remaining = Number(currentBagValue) - Number(payload.entry_value);
+            consumed = Number(payload.entry_value)
+        } else if (payload.entry_type === "returned") {
+            remaining = Number(currentBagValue) - Number(payload.entry_value);
+            returned = Number(payload.entry_value)
+        } else {
+            remaining = Number(currentBagValue)
+        }
+        if (remaining < 0) {
+            throw ("problem while calculating remaining value")
+        }
+        return {
+            remaining: remaining,
+            consumed: consumed,
+            taken: taken,
+            returned: returned
+        }
+    } catch (error) {
+        throw ("remaing stock canot be negative")
     }
-await Stock.findOne({product_id:req.body.product_id,user_id:req.body.user_id  }).exec().then(result => {
-    stock = result;
-    result.bag_value = remaining;
-    result.save()
-}).catch(error => {
-    console.log(error)
+}
+
+const addEntry = async (payload, res, next) => {
+    try {
+        let remaining = 0;
+        let product = await Product.findById(payload.product_id)
+        let user = await User.findById(payload.user_id);
+        let stock = await Stock.findOne({ product_id: payload.product_id, user_id: payload.user_id });
+        const calculatedBagValues = getBagValue(payload, stock.bag_value, res);
+        console.log(calculatedBagValues.remaining)
+        stock.bag_value = calculatedBagValues.remaining;
+        let stockSave = await stock.save();
+        const entry = new Entry({
+            _id: new mongoose.Types.ObjectId(),
+            user_id: payload.user_id,
+            product_id: payload.product_id,
+            product_name: product.name,
+            user_name: user.name,
+            entry_type: payload.entry_type,
+            entry_value: payload.entry_value,
+            taken: calculatedBagValues.taken || 0,
+            consumed: calculatedBagValues.consumed || 0,
+            returned: calculatedBagValues.returned || 0,
+            remaining: Number(calculatedBagValues.remaining),
+            created_at: moment().format("DD-MM-YYYY HH:mm:ss")
+        });
+        return { remaining: remaining, entry: entry };
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+router.get(("/"), async (req, res, next) => {
+    try {
+        const entries = await Entry.find();
+        res.status(200).json(formatResponse(true, "entries retrieved successfully", { entries: entries }));
+    } catch (error) {
+        res.status(500).json(formatResponse(false, `error occured while retrieving entries: ${error}`))
+    }
 });
 
-    const entry = new Entry({
-        _id:new mongoose.Types.ObjectId(),
-        user_id:req.body.user_id,
-        product_id:req.body.product_id,
-        product_name:product.name,
-        user_name:user.name,
-        entry_type:req.body.entry_type,
-        entry_value:req.body.entry_value,
-        taken:taken || 0,
-        consumed:consumed || 0,
-        returned:returned || 0,
-        remaining:Number(remaining),
-        created_at: moment().format("DD-MM-YYYY hh:mm A")
-    })
-    entry.save().then((result) => {
-        res.status(201).json(formatResponse(true,"entry created successfully",{createdEntry:entry}));
-    }).catch(error => {
-        res.status(500).json(formatResponse(false,`error while creating entry: ${error}`));        
-    });
+router.post(("/"), async (req, res, next) => {
+    try {
+        const result = await addEntry(req.body, res, next);
+        const saveEntry = await result.entry.save();
+        res.status(201).json(formatResponse(true, "entry created successfully", { createdEntry: saveEntry }));
+    } catch (error) {
+        res.status(500).json(formatResponse(false, `error while creating entry: ${error}`));
+    }
 });
 
-router.delete(("/:entryId"),(req, res, next) => {
+router.delete(("/:entryId"), (req, res, next) => {
     const id = req.params.entryId;
     Entry.findByIdAndRemove(id).exec().then(response => {
         res.status(200).json({
-            message:"entry deleted successsfully",
+            message: "entry deleted successsfully",
             id: response.id
         });
     }).catch(error => {
-        res.status(500).json(formatResponse(false,`error while deleting entry: ${error}`));
+        res.status(500).json(formatResponse(false, `error while deleting entry: ${error}`));
     })
 });
 
-// router.get(("/:productId"),(req, res, next) => {
-//     const id = req.params.productId;
-//     Product.findById(id).exec().then(product => {
-//         console.log(product)
-//         res.status(200).json({product:product});
-//     }).catch(error => {console.log(error)})
-// });
+router.put(("/"), async (req, res, next) => {
+    try {
+        const sortedEntries = await Entry.aggregate(
+            [{
+                $match: {
+                    user_id: req.body.user_id,
+                    product_id: req.body.product_id,
+                    created_at: { $gte: req.body.created_at }
+                }
+            },
+                // {$group:{
+                //     _id:"$_id",total:{$sum:{$add: ["$consumed", "$returned"]}}
+                // }},
 
-// router.patch(("/:productId"),(req, res, next) => {
-//     let message = "product edited"
-//     if(req.params.productId === "specific"){
-//         message = "specific product edited"
-//     }
-//     res.status(200).json({
-//         messaage:message
-//     });
-// });
+                // { $project: {
+                //     total: { $add: ["$consumed", "$returned"] }
+                // }}
+            ]
+        )
+        if (sortedEntries.length !== 0) {
+            let prevBagValue = null;
+            if (sortedEntries.length !== 0 && sortedEntries[0].entry_type === "taken") {
+                prevBagValue = sortedEntries[0].remaining - sortedEntries[0].taken
+            } else {
+                prevBagValue = sortedEntries[0].remaining + sortedEntries[0][sortedEntries[0]["entry_type"]]
+            }
+            console.log("prevBagValue:", prevBagValue)
+            if (prevBagValue < 0) {
+                throw ("Negative stock")
+            }
+            const rowsToBeInserted = sortedEntries.reduce((acc, item, index) => {
+                let updatedItem = [];
+                if (index === 0) {
+                    item["entry_type"] = req.body.entry_type;
+                    item["entry_value"] = req.body.entry_value;
+                    updatedItem = {
+                        ...item,
+                        ...getBagValue(item, prevBagValue),
+                    }
+                } else {
+                    updatedItem = {
+                        ...item,
+                        ...getBagValue(item, acc[index - 1].remaining),
+                    }
+                }
+                acc.push(updatedItem);
+                return acc;
+            }, []);
+            const stock = await Stock.findOne({ product_id: sortedEntries[0].product_id, user_id: sortedEntries[0].user_id })
+            const lastRecord = rowsToBeInserted.slice()[0];
+            stock.bag_value = lastRecord.remaining;
+            const saveStock = await stock.save();
+            const recordsTobeDeleted = await rowsToBeInserted.map((item) => {
+                return item._id
+            });
+            const deletedEntries = await Entry.deleteMany({ _id: { $in: recordsTobeDeleted } });
+            const insertedRows = await Entry.insertMany(rowsToBeInserted);
+            res.status(201).json(formatResponse(true, `entries created successfully`));
+        } else {
+            res.status(500).json(formatResponse(false, "no matching records found"))
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(formatResponse(false, `network error occured ${error}`))
+    }
+})
 
 module.exports = router;
